e9891093a7abfe191490a56e32fdab205da2cda0
dshakalo <aditor@gmail.com>
2025-10-18 17:45:03 +0300

    instance id output added

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "bastion_instance_profile_name" {
   value       = try(aws_iam_instance_profile.bastion[0].name, null)
   description = "Instance profile name attached to bastion (null if not created)."
 }
+
+output "bastion_id" {
+  value       = aws_instance.bastion.id
+  description = "Bastion host EC2 instance ID (null if not created)."
+}
