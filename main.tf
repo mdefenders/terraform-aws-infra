ff096b0cc77adb586fa89e8fd1e6ed8bd9f3ed33
dshakalo <aditor@gmail.com>
2025-10-19 07:42:58 +0300

    user data added

--- main.tf
+++ main.tf
@@ -103,6 +103,9 @@ resource "aws_instance" "bastion" {
     sudo yum -y install terraform
     sudo yum install -y tmux
     sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/
   EOF
 
   metadata_options {
